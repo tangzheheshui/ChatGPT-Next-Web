c237c9aa43b179637baada46e1ec0d2bf7a19bbc
tangzheheshui <35756419+tangzheheshui@users.noreply.github.com>
2024-01-21 04:18:11 +0000

    modify title 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "NextChat1",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
